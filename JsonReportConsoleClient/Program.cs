1fc3b20043cd252e37ccffa9b5ea623881c48ca7
Gencho <Rasilev>
2014-09-02 11:30:57 +0300

    Add not working Json reporter project.

---
+++ JsonReportConsoleClient/Program.cs
@@ -0,0 +1,46 @@
+﻿using JsonReportModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Telerik.OpenAccess;
+
+namespace JsonReportConsoleClient
+{
+    class Program
+    {
+        static void Main()
+        {
+            UpdateDatabase();
+        }
+
+        private static void UpdateDatabase()
+        {
+            using (var context = new JsonReportModel.FluentModel())
+            {
+                var schemaHandler = context.GetSchemaHandler();
+                EnsureDB(schemaHandler);
+            }
+        }
+
+        private static void EnsureDB(ISchemaHandler schemaHandler)
+        {
+            string script = null;
+            if (schemaHandler.DatabaseExists())
+            {
+                script = schemaHandler.CreateUpdateDDLScript(null);
+            }
+            else
+            {
+            schemaHandler.CreateDatabase();
+            script = schemaHandler.CreateDDLScript();
+            }
+
+            if (!string.IsNullOrEmpty(script))
+            {
+                schemaHandler.ExecuteDDLScript(script);
+            }
+        }
+    }
+}
---
+++ JsonReportModel/Report.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JsonReportModel
+{
+    public class Report
+    {
+        public int RepodrtID { get; set; }
+
+        public string From { get; set; }
+
+        public string To { get; set; }
+
+        public int SellTiketsCount { get; set; }
+
+        public int Year { get; set; }
+
+    }
+}
