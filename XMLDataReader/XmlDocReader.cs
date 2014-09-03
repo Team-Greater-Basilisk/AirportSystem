304f7bdc4d1063bf5889834f514b5d02deb9b941
yavor <iavorsk@gmail.com>
2014-09-03 14:50:21 +0300

    XmlDocReader edit

--- XMLDataReader/XmlDocReader.cs
+++ XMLDataReader/XmlDocReader.cs
@@ -1,5 +1,6 @@
 ﻿using Airport.Data;
 using Airport.Model;
+using MongoDBController;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,19 +14,90 @@ namespace XMLDataReader
     {
         public void ReadXmlAndSendToMsSQL(string filename, AirportDbContext dbContext)
         {
-            //
-            List<Ticket> ticketsFromXml = this.ReadXML(filename);
+            List<object> data = this.ReadXML(filename);
 
-            foreach (var ticket in ticketsFromXml)
+            if (data.Count > 0 && (data[0] is Ticket))
             {
-                dbContext.Tickets.Add(ticket);
+                foreach (var ticket in data)
+                {
+                    Ticket newTicketEntry = ticket as Ticket;
+                    dbContext.Tickets.Add(newTicketEntry);
+                }
+            }
+
+            if (data.Count > 0 && (data[0] is CompanyInfo))
+            {
+                foreach (var cInfo in data)
+                {
+                    CompanyInfo newCompanyInfo = cInfo as CompanyInfo;
+                    dbContext.CompanyIfno.Add(newCompanyInfo);
+                }
+            }
+
+            dbContext.SaveChanges();
+        }
+
+        public void ReadXmlAndSendToMongoDb(string filename, MongoDataInserter mongoDatabase)
+        {
+            List<object> data = this.ReadXML(filename);
+
+            if (data.Count > 0 && (data[0] is Ticket))
+            {
+                foreach (var ticket in data)
+                {
+                    Ticket newTicketEntry = ticket as Ticket;
+
+                    Company company = newTicketEntry.Company;
+                    mongoDatabase.AddComapny(company);
+
+                    Customer customer = newTicketEntry.Customer;
+                    mongoDatabase.AddComapny(customer);
+
+                    Destination destination = newTicketEntry.Destination;
+                    mongoDatabase.AddComapny(destination);
+                }
+            }
+
+            if (data.Count > 0 && (data[0] is CompanyInfo))
+            {
+                foreach (var ticket in data)
+                {
+                    CompanyInfo newCompanyInfo = cInfo as CompanyInfo;
+
+                    //mongoDatabase.AddComapnyInfo(newCompanyInfo);
+                }
             }
         }
 
-        private List<Ticket> ReadXML(string filename)
+        private List<object> ReadXML(string filename)
         {
             XmlReader reader = XmlReader.Create(filename);
-            List<Ticket> ticketsList = new List<Ticket>();
+            List<object> resultList = new List<object>();
+
+            using (reader)
+            {
+                while (reader.Read())
+                {
+                    if ((reader.NodeType == XmlNodeType.Element) && (reader.Name == "tickets"))
+                    {
+                        resultList = ReadTicketsInfoXML(reader);
+                        break;
+                    }
+
+                    if ((reader.NodeType == XmlNodeType.Element) && (reader.Name == "additional-info"))
+                    {
+                        resultList = ReadAdditionalInfoXML(reader);
+                        break;
+                    }
+                }
+            }
+
+            return resultList;
+        }
+
+        private List<object> ReadTicketsInfoXML(XmlReader reader)
+        {
+            List<object> ticketsList = new List<object>();
 
             using (reader)
             {
@@ -40,7 +112,7 @@ namespace XMLDataReader
 
                     if ((reader.NodeType == XmlNodeType.EndElement) && (reader.Name == "ticket"))
                     {
-                        ticketsList.Add(newTicket);
+                        ticketsList.Add((object)newTicket);
                     }
 
                     if ((reader.NodeType == XmlNodeType.Element) && (reader.Name == "company"))
@@ -95,6 +167,65 @@ namespace XMLDataReader
             }
 
             return ticketsList;
+        }
+
+        private List<object> ReadAdditionalInfoXML(XmlReader reader)
+        {
+            List<object> additionalInfo = new List<object>();
+
+            using (reader)
+            {
+                while (reader.Read())
+                {
+                    if ((reader.NodeType == XmlNodeType.Element) && (reader.Name == "company"))
+                    {
+                        string companyName = reader["name"];
+                        string companyCountry = string.Empty;
+                        string companyWebSite = string.Empty;
+                        int numberOfEmployees = 0;
+
+                        while (!((reader.NodeType == XmlNodeType.EndElement) && (reader.Name == "company")))
+                        {
+                            if ((reader.NodeType == XmlNodeType.Element) && (reader.Name == "country"))
+                            {
+                                if (reader.Read())
+                                {
+                                    companyCountry = reader.Value;
+                                }
+                            }
+
+                            if ((reader.NodeType == XmlNodeType.Element) && (reader.Name == "website"))
+                            {
+                                if (reader.Read())
+                                {
+                                    companyWebSite = reader.Value;
+                                }
+                            }
+
+                            if ((reader.NodeType == XmlNodeType.Element) && (reader.Name == "number-of-employees"))
+                            {
+                                if (reader.Read())
+                                {
+                                    numberOfEmployees = int.Parse(reader.Value);
+                                }
+                            }
+
+                            reader.Read();
+                        }
+
+                        CompanyInfo coInfo = new CompanyInfo();
+
+                        coInfo.CompanyNameInfo = companyName;
+                        coInfo.CompanyCountryInfo = companyCountry;
+                        coInfo.CompanyWebSiteInfo = companyWebSite;
+                        coInfo.CompanyNumberOfEmployees = numberOfEmployees;
+
+                        additionalInfo.Add((object)coInfo);
+                    }
+                }
+            }
+
+            return additionalInfo;
         }
     }
 }
